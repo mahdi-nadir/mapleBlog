d6f4a5bde65516370a6079e994dbc63e0e829163
Mahdi NADIR <e2194498@cmaisonneuve.qc.ca>
2023-12-16 18:16:50 -0500

    add message date // add to_show to the table

--- resources/views/profile/messages/messages.blade.php
+++ resources/views/profile/messages/messages.blade.php
@@ -5,6 +5,41 @@
             <div>
                 {{ $message->fromUser->username }} 
                 {{ $message->content }}
+                @php
+                    $dateString = $message->created_at;
+                    $date = new DateTime($dateString);
+                    $now = new DateTime();
+                    $interval = $now->diff($date);
+                    if (LaravelLocalization::getCurrentLocale() == 'en') {
+                        if ($interval->y > 0) {
+                            echo $interval->y . ' years ago';
+                        } elseif ($interval->m > 0) {
+                            echo $interval->m . ' months ago';
+                        } elseif ($interval->d > 0) {
+                            echo $interval->d . ' days ago';
+                        } elseif ($interval->h > 0) {
+                            echo $interval->h . ' hours ago';
+                        } elseif ($interval->i > 0) {
+                            echo $interval->i . ' minutes ago';
+                        } elseif ($interval->s > 0) {
+                            echo $interval->s . ' seconds ago';
+                        }
+                    } else {
+                        if ($interval->y > 0) {
+                            echo $interval->y . ' ans';
+                        } elseif ($interval->m > 0) {
+                            echo $interval->m . ' mois';
+                        } elseif ($interval->d > 0) {
+                            echo $interval->d . ' jours';
+                        } elseif ($interval->h > 0) {
+                            echo $interval->h . ' heures';
+                        } elseif ($interval->i > 0) {
+                            echo $interval->i . ' minutes';
+                        } elseif ($interval->s > 0) {
+                            echo $interval->s . ' secondes';
+                        }
+                    }
+                @endphp
             </div>
             @endforeach
         </div>
